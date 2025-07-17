be95c6311c24c82bc271a599da059753ed509127
Nick Heyek <nheyek@pm.me>
2025-07-17 13:57:10 -0500

    Add js package, express boilerplate

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Not found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
