d0db71c279f4dfaf8ad16fe10d8ef662f64bcbcd
Nick Heyek <nick@heyek.com>
2025-09-19 14:47:09 -0500

    Include index.html, improve production front-end build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,55 +5,62 @@ require('dotenv').config();
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  devtool: 'source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'public'),
-    historyApiFallback: true,
-    port: 3000,
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'http://localhost:8000',
-        changeOrigin: true
-      },
-      {
-        context: ['/*.png'],
-        target: 'http://localhost:8000',
-        changeOrigin: true
-      }
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
-  ]
+module.exports = (env, argv) => {
+	const isProd = argv.mode === 'production';
+
+	return {
+		mode: isProd ? 'production' : 'development',
+		entry: './src/index.tsx',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.[contenthash].js',
+			clean: true,
+			publicPath: '/',
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js'],
+		},
+		devtool: 'source-map',
+		devServer: {
+			static: path.resolve(__dirname, 'public'),
+			historyApiFallback: true,
+			port: 3000,
+			proxy: [
+				{
+					context: ['/api'],
+					target: 'http://localhost:8000',
+					changeOrigin: true,
+				},
+				{
+					context: ['/*.png'],
+					target: 'http://localhost:8000',
+					changeOrigin: true,
+				},
+			],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './public/index.html',
+			}),
+			new webpack.DefinePlugin({
+				'process.env': JSON.stringify(process.env),
+			}),
+		],
+		optimization: {
+			minimize: isProd,
+		},
+	};
 };
