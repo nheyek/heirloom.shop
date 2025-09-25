cdec585895eeddd2cd0ecc505f0e6a79519feb58
Nick Heyek <nick@heyek.com>
2025-09-25 12:57:31 -0500

    DB access script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -93,11 +93,6 @@ resource "digitalocean_database_user" "app_user" {
   name       = "heirloom"
 }
 
-resource "digitalocean_database_user" "github_user" {
-  cluster_id = digitalocean_database_cluster.db.id
-  name       = "github"
-}
-
 resource "digitalocean_database_db" "app_db" {
   cluster_id = digitalocean_database_cluster.db.id
   name       = "heirloomdb"
