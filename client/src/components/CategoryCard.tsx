e5a543a13202cb743a8dbe89f28ccc86ae166555
Nick Heyek <nick@heyek.com>
2025-12-04 08:27:25 -0600

    Tweak category card styling

--- client/src/components/CategoryCard.tsx
+++ client/src/components/CategoryCard.tsx
@@ -4,12 +4,15 @@ import { CategoryCardData } from '@common/types/CategoryCardData';
 export const CategoryCard = (props: CategoryCardData) => {
 	return (
 		<Box
-			borderRadius={5}
+			borderRadius="5px"
 			overflow="hidden"
 			position="relative"
 			cursor="pointer"
 			aspectRatio={3 / 2}
 			width="100%"
+			style={{
+				boxShadow: 'rgba(0, 0, 0, 0.25) 0px 1px 3px, rgba(0, 0, 0, 0.125) 0px 1px 2px',
+			}}
 		>
 			<Image
 				height="100%"
@@ -19,18 +22,18 @@ export const CategoryCard = (props: CategoryCardData) => {
 			></Image>
 			<Heading
 				position="absolute"
-				bottom="5px"
-				left="5px"
-				paddingLeft="10px"
+				bottom="0"
+				left="0"
+				paddingLeft="15px"
 				paddingRight="10px"
-				paddingBottom="2.5px"
+				paddingBottom="7.5px"
 				paddingTop="2.5px"
-				borderRadius="full"
+				borderRadius="0 5px 0 0"
 				fontSize="22px"
 				fontWeight="500"
 				style={{
 					color: '#FFF',
-					backdropFilter: 'blur(2.5px)',
+					backdropFilter: 'blur(5px)',
 				}}
 			>
 				{props.title}
