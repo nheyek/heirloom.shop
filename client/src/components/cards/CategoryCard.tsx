47e60c43d075923c49dec2bfcb7c337d7b040b57
Nick Heyek <nick@heyek.com>
2026-01-02 16:30:03 -0600

    Mobile layout tweaks

--- client/src/components/cards/CategoryCard.tsx
+++ client/src/components/cards/CategoryCard.tsx
@@ -27,7 +27,8 @@ export const CategoryCard = (props: CategoryCardData) => {
 				left="15px"
 				padding={0}
 				lineHeight={1}
-				fontSize={{ base: '28px', md: '22px', lg: '28px', xl: '36px' }}
+				truncate
+				fontSize={{ base: '26px', md: '22px', lg: '28px', xl: '36px' }}
 				fontWeight="700"
 				color="#FFF"
 				textShadow="0 1px 2px rgba(0, 0, 0, 0.5), 0 2px 4px rgba(0, 0, 0, 0.35), 0 4px 8px rgba(0, 0, 0, 0.25), 0 8px 16px rgba(0, 0, 0, 0.15);"
