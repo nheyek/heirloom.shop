925d590159bdad47052995b823748132b1d8243e
Nick Heyek <nick@heyek.com>
2026-01-07 13:59:19 -0600

    Light box component implementation

--- client/src/components/misc/ImageCollage.tsx
+++ client/src/components/misc/ImageCollage.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Grid, GridItem, Image, useBreakpointValue } from '@chakra-ui/react';
+import { useState } from 'react';
 import { FaImages } from 'react-icons/fa';
+import { LightBox } from './LightBox';
 
 type Props = {
 	aspectRatio: number;
@@ -14,45 +16,65 @@ export const ImageCollage = (props: Props) => {
 	const thumnNailImageUrls = props.urls.slice(1, numGridCols * 2 - 1);
 	const imageListTruncated = props.urls.length > 1 + thumnNailImageUrls.length;
 
+	const [lightBoxPage, setLightBoxPage] = useState<number | null>(null);
+
 	return (
-		<Box position="relative">
-			<Grid
-				aspectRatio={props.urls.length > 1 ? props.aspectRatio * 2 : props.aspectRatio}
-				templateRows="repeat(2, 1fr)"
-				templateColumns={`repeat(${numGridCols + 1}, 1fr)`}
-				maxH={500}
-				gap={3}
-			>
-				<GridItem
-					rowSpan={2}
-					colSpan={2}
-					aspectRatio={props.aspectRatio}
-					width="100%"
-					height="100%"
+		<>
+			<LightBox {...props} page={lightBoxPage} setPage={setLightBoxPage} />
+			<Box position="relative">
+				<Grid
+					aspectRatio={props.urls.length > 1 ? props.aspectRatio * 2 : props.aspectRatio}
+					templateRows="repeat(2, 1fr)"
+					templateColumns={`repeat(${numGridCols + 1}, 1fr)`}
+					maxH={500}
+					gap={3}
 				>
-					<GridImage src={props.urls[0]} aspectRatio={props.aspectRatio} />
-				</GridItem>
-				{thumnNailImageUrls.map((url) => (
-					<GridImage src={url} aspectRatio={props.aspectRatio} />
-				))}
-			</Grid>
-			{imageListTruncated && (
-				<Button variant="subtle" position="absolute" right={3} bottom={3} fontWeight="bold">
-					<FaImages />
-					{props.urls.length} images
-				</Button>
-			)}
-		</Box>
+					<GridItem
+						rowSpan={2}
+						colSpan={2}
+						aspectRatio={props.aspectRatio}
+						width="100%"
+						height="100%"
+					>
+						<GridImage
+							src={props.urls[0]}
+							aspectRatio={props.aspectRatio}
+							onClick={() => setLightBoxPage(0)}
+						/>
+					</GridItem>
+					{thumnNailImageUrls.map((url, index) => (
+						<GridImage
+							src={url}
+							aspectRatio={props.aspectRatio}
+							onClick={() => setLightBoxPage(index + 1)}
+						/>
+					))}
+				</Grid>
+				{imageListTruncated && (
+					<Button
+						variant="subtle"
+						position="absolute"
+						right={3}
+						bottom={3}
+						fontWeight="bold"
+						onClick={() => setLightBoxPage(0)}
+					>
+						<FaImages />
+						{props.urls.length} images
+					</Button>
+				)}
+			</Box>
+		</>
 	);
 };
 
-const GridImage = (props: { src: string; aspectRatio: number }) => (
+const GridImage = (props: { src: string; aspectRatio: number; onClick: () => void }) => (
 	<Image
 		{...props}
 		width="100%"
 		height="100%"
 		objectFit="cover"
 		borderRadius={5}
-		boxShadow="md"
+		cursor="pointer"
 	/>
 );
---
+++ client/src/components/misc/LightBox.tsx
@@ -0,0 +1,119 @@
+import {
+	AspectRatio,
+	Carousel,
+	Dialog,
+	HStack,
+	IconButton,
+	IconButtonProps,
+	Image,
+	Portal,
+	useCarouselContext,
+} from '@chakra-ui/react';
+
+import { IoClose } from 'react-icons/io5';
+import { LuChevronLeft, LuChevronRight } from 'react-icons/lu';
+
+type Props = {
+	urls: string[];
+	setPage: (page: number | null) => void;
+	page: number | null;
+	aspectRatio: number;
+};
+
+export const LightBox = (props: Props) => {
+	return (
+		<Dialog.Root size="full" open={props.page !== null}>
+			<Portal>
+				<Dialog.Backdrop />
+				<Dialog.Positioner>
+					<Dialog.Content bg="transparent" shadow="none">
+						<Dialog.Body
+							display="flex"
+							alignItems="center"
+							justifyContent="center"
+							p={0}
+						>
+							<Carousel.Root
+								slideCount={props.urls.length}
+								page={props.page || 0}
+								onPageChange={(e) => props.setPage(e.page)}
+							>
+								<ActionButton
+									onClick={() => props.setPage(null)}
+									top={5}
+									right={5}
+									size="xs"
+								>
+									<IoClose />
+								</ActionButton>
+								<Carousel.Control>
+									<Carousel.PrevTrigger asChild insetStart={10}>
+										<ActionButton>
+											<LuChevronLeft />
+										</ActionButton>
+									</Carousel.PrevTrigger>
+
+									<Carousel.ItemGroup
+										aspectRatio={props.aspectRatio}
+										height="80vh"
+									>
+										{props.urls.map((src, index) => (
+											<Carousel.Item
+												key={index}
+												index={index}
+												overflow="hidden"
+											>
+												<Image
+													src={src}
+													objectFit="cover"
+													aspectRatio={props.aspectRatio}
+													borderRadius={5}
+												/>
+											</Carousel.Item>
+										))}
+									</Carousel.ItemGroup>
+
+									<Carousel.NextTrigger asChild insetEnd={10}>
+										<ActionButton>
+											<LuChevronRight />
+										</ActionButton>
+									</Carousel.NextTrigger>
+								</Carousel.Control>
+
+								<CarouselThumbnails
+									urls={props.urls}
+									aspectRatio={props.aspectRatio}
+								/>
+							</Carousel.Root>
+						</Dialog.Body>
+					</Dialog.Content>
+				</Dialog.Positioner>
+			</Portal>
+		</Dialog.Root>
+	);
+};
+
+const CarouselThumbnails = (props: { urls: string[]; aspectRatio: number }) => {
+	const carousel = useCarouselContext();
+
+	return (
+		<HStack justify="center">
+			<Carousel.ProgressText mr={5} fontWeight="bold" />
+			{props.urls.map((src, index) => (
+				<AspectRatio
+					key={index}
+					ratio={props.aspectRatio}
+					width={20}
+					cursor="button"
+					onClick={() => carousel.scrollTo(index)}
+				>
+					<Image src={src} w="100%" h="100%" objectFit="cover" borderRadius={5} />
+				</AspectRatio>
+			))}
+		</HStack>
+	);
+};
+
+const ActionButton = (props: IconButtonProps) => (
+	<IconButton size="sm" variant="subtle" borderRadius="full" position="absolute" {...props} />
+);
