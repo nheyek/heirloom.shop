901f74257b22d1184345e00a63e9dc20c70a4ff1
Nick Heyek <nick@heyek.com>
2026-01-28 09:54:48 -0600

    Only render app when breakpoints are ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useBreakpointValue } from '@chakra-ui/react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ScrollToTop } from './components/misc/ScrollToTop';
 import { Navbar } from './components/navbar/NavBar';
@@ -10,22 +10,29 @@ import { ListingPage } from './pages/ListingPage';
 import { ShopManager } from './pages/ShopManager';
 import { ShopPage } from './pages/ShopPage';
 
-const App = () => (
-	<>
-		<ScrollToTop />
-		<Navbar />
-		<Box maxWidth={1600} mx="auto">
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/callback" element={<AuthCallback />} />
-				<Route path={`/${CLIENT_ROUTES.shopManager}`} element={<ShopManager />} />
-				<Route path={`/${CLIENT_ROUTES.category}/:id`} element={<CategoryPage />} />
-				<Route path={`/${CLIENT_ROUTES.shop}/:id`} element={<ShopPage />} />
-				<Route path={`/${CLIENT_ROUTES.listing}/:id`} element={<ListingPage />} />
-				<Route path="*" element={<Navigate to="/" replace />} />
-			</Routes>
-		</Box>
-	</>
-);
+const App = () => {
+	const breakpoint = useBreakpointValue({ base: true });
+	if (!breakpoint) {
+		return null;
+	}
+
+	return (
+		<>
+			<ScrollToTop />
+			<Navbar />
+			<Box maxWidth={1600} mx="auto">
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/callback" element={<AuthCallback />} />
+					<Route path={`/${CLIENT_ROUTES.shopManager}`} element={<ShopManager />} />
+					<Route path={`/${CLIENT_ROUTES.category}/:id`} element={<CategoryPage />} />
+					<Route path={`/${CLIENT_ROUTES.shop}/:id`} element={<ShopPage />} />
+					<Route path={`/${CLIENT_ROUTES.listing}/:id`} element={<ListingPage />} />
+					<Route path="*" element={<Navigate to="/" replace />} />
+				</Routes>
+			</Box>
+		</>
+	);
+};
 
 export { App };
