78c3227ba37c7a1e47c28a68c66d341b5525e14d
Nick Heyek <nick@heyek.com>
2026-01-28 09:38:04 -0600

    Improve loading UI for category grid

--- client/src/components/grids/CategoryGrid.tsx
+++ client/src/components/grids/CategoryGrid.tsx
@@ -1,6 +1,9 @@
-import { GridItem, SimpleGrid, Skeleton, useBreakpointValue } from '@chakra-ui/react';
+import { SimpleGrid, Skeleton, useBreakpointValue } from '@chakra-ui/react';
 import { CategoryTileData } from '@common/types/CategoryTileData';
+import { STANDARD_IMAGE_ASPECT_RATIO } from '../../constants';
 import { CategoryTile } from '../CategoryTile';
+
+const DEFAULT_NUM_PLACEHOLDERS = 4;
 
 type Props = {
 	isLoading: boolean;
@@ -9,20 +12,13 @@ type Props = {
 };
 
 export const CategoryGrid = (props: Props) => {
-	if (props.isLoading) {
-		return <Skeleton width="100%" height={225} borderRadius={0} />;
-	}
-
-	if (!props.categories.length) {
-		return null;
-	}
-
 	const cols = { base: 2, md: 3, lg: 4 };
 	const maxNumColumns = useBreakpointValue(cols) || 2;
 
-	const widthPercent = props.isLoading
-		? 100
-		: Math.min(100, 100 * (props.categories.length / maxNumColumns));
+	const numSkeletons = props.numPlaceholders || DEFAULT_NUM_PLACEHOLDERS;
+
+	const numTiles = props.isLoading ? numSkeletons : props.categories.length;
+	const widthPercent = Math.min(100, 100 * (numTiles / maxNumColumns));
 
 	return (
 		<SimpleGrid
@@ -30,11 +26,17 @@ export const CategoryGrid = (props: Props) => {
 			overflow="hidden"
 			width={`${widthPercent}%`}
 		>
-			{props.categories.map((category) => (
-				<GridItem flex="1">
-					<CategoryTile key={category.id} {...category} />
-				</GridItem>
-			))}
+			{props.isLoading
+				? Array.from({ length: numSkeletons }).map((_, index) => (
+						<Skeleton
+							width="100%"
+							aspectRatio={STANDARD_IMAGE_ASPECT_RATIO}
+							borderRadius={0}
+						/>
+					))
+				: props.categories.map((category) => (
+						<CategoryTile key={category.id} {...category} />
+					))}
 		</SimpleGrid>
 	);
 };
