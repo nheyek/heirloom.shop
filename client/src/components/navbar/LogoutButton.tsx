89955825ede20ce3073f30d79ad5e2fc7ce6d154
Nick Heyek <nick@heyek.com>
2026-01-30 14:53:50 -0600

    Add framer-motion animation with MotionBox for menu

--- client/src/components/navbar/LogoutButton.tsx
+++ client/src/components/navbar/LogoutButton.tsx
@@ -1,11 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, Menu, Portal } from '@chakra-ui/react';
+import { Box, Button, Menu, Portal } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import React from 'react';
 import { FaHeart, FaUserCircle } from 'react-icons/fa';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { PiSignOutBold } from 'react-icons/pi';
 import { useNavigate } from 'react-router-dom';
 import { CLIENT_ROUTES } from '../../constants';
+
+const MotionBox = motion.create(Box);
 
 export const LogoutButton = () => {
 	const { logout } = useAuth0();
@@ -29,28 +32,35 @@ export const LogoutButton = () => {
 			</Menu.Trigger>
 			<Portal>
 				<Menu.Positioner>
-					<Menu.Content boxShadow="md" animation="fade-in" animationDuration="fast">
-						<Menu.Item
-							value="saved"
-							onClick={() => navigate(`/${CLIENT_ROUTES.saved}`)}
-							cursor="pointer"
-							fontSize={16}
-							py={2}
-						>
-							<FaHeart />
-							Saved Listings
-						</Menu.Item>
-						<Menu.Item
-							value="logout"
-							onClick={handleLogout}
-							cursor="pointer"
-							fontSize={16}
-							py={2}
-						>
-							<PiSignOutBold />
-							Log out
-						</Menu.Item>
-					</Menu.Content>
+					<MotionBox
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.15 }}
+					>
+						<Menu.Content boxShadow="md">
+							<Menu.Item
+								value="saved"
+								onClick={() => navigate(`/${CLIENT_ROUTES.saved}`)}
+								cursor="pointer"
+								fontSize={16}
+								py={2}
+							>
+								<FaHeart />
+								Saved Listings
+							</Menu.Item>
+							<Menu.Item
+								value="logout"
+								onClick={handleLogout}
+								cursor="pointer"
+								fontSize={16}
+								py={2}
+							>
+								<PiSignOutBold />
+								Log out
+							</Menu.Item>
+						</Menu.Content>
+					</MotionBox>
 				</Menu.Positioner>
 			</Portal>
 		</Menu.Root>
