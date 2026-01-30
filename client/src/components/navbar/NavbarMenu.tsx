55ba45b148b61b3e3a4cddd4bcf422293b139174
Nick Heyek <nick@heyek.com>
2026-01-30 15:56:34 -0600

    Add NavbarMenu component (renamed from LogoutButton) using AnimatedDropdown

---
+++ client/src/components/navbar/NavbarMenu.tsx
@@ -0,0 +1,58 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button, Menu } from '@chakra-ui/react';
+import { FaHeart, FaUserCircle } from 'react-icons/fa';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import { PiSignOutBold } from 'react-icons/pi';
+import { useNavigate } from 'react-router-dom';
+import { CLIENT_ROUTES } from '../../constants';
+import { AnimatedDropdown } from './AnimatedDropdown';
+
+export const NavbarMenu = () => {
+	const { logout } = useAuth0();
+	const navigate = useNavigate();
+
+	const handleLogout = async () => {
+		logout({
+			logoutParams: {
+				returnTo: window.location.origin,
+			},
+		});
+	};
+
+	return (
+		<Menu.Root>
+			<Menu.Trigger asChild focusRing="none">
+				<Button variant="plain" size="lg" pl="10px" pr="0" gap="2px">
+					<FaUserCircle />
+					<IoMdArrowDropdown />
+				</Button>
+			</Menu.Trigger>
+			<Menu.Positioner>
+				<AnimatedDropdown isOpen={true} position="relative">
+					<Menu.Content boxShadow="none">
+						<Menu.Item
+							value="saved"
+							onClick={() => navigate(`/${CLIENT_ROUTES.saved}`)}
+							cursor="pointer"
+							fontSize={16}
+							py={2}
+						>
+							<FaHeart />
+							Saved Listings
+						</Menu.Item>
+						<Menu.Item
+							value="logout"
+							onClick={handleLogout}
+							cursor="pointer"
+							fontSize={16}
+							py={2}
+						>
+							<PiSignOutBold />
+							Log out
+						</Menu.Item>
+					</Menu.Content>
+				</AnimatedDropdown>
+			</Menu.Positioner>
+		</Menu.Root>
+	);
+};
