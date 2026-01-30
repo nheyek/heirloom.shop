64b51e2f36d66c010f067b15fe4a8e0271733d40
Nick Heyek <nick@heyek.com>
2026-01-30 09:20:20 -0600

    Update me routes to use favoriteListing controller

--- server/src/routes/me.routes.ts
+++ server/src/routes/me.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { getCurrentUser } from '../controllers/user.controller';
-import { getSavedListings } from '../controllers/savedListing.controller';
+import { getFavoritedListings } from '../controllers/favoriteListing.controller';
 import { authAndSetUser } from '../middleware/auth0.middleware';
 
 const router = Router();
 
 router.get('/', authAndSetUser, getCurrentUser);
-router.get('/saved-listings', authAndSetUser, getSavedListings);
+router.get('/saved-listings', authAndSetUser, getFavoritedListings);
 
 export default router;
