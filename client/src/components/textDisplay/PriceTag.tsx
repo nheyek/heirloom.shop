bb0f6a1d225869d39be471426315c81293b741f6
Nick Heyek <nick@heyek.com>
2026-03-17 15:13:19 -0500

    Update PriceTag to accept cents and convert at display

--- client/src/components/textDisplay/PriceTag.tsx
+++ client/src/components/textDisplay/PriceTag.tsx
@@ -2,44 +2,48 @@ import { Flex, Text } from '@chakra-ui/react';
 import { FONT_DECORATIVE } from '../../theme';
 
 type Props = {
-	value: number;
+	priceCents: number;
 	quantity?: number;
 };
 
-export const PriceTag = (props: Props) => (
-	<Flex
-		position="relative"
-		borderRadius={5}
-		bg="brand"
-		color="#FFFFFF"
-		style={{
-			clipPath:
-				'polygon(0 50%, 10px 100%, 100% 100%, 100% 0, 10px 0)',
-		}}
-		_after={{
-			content: '""',
-			position: 'absolute',
-			left: '10px',
-			top: '50%',
-			transform: 'translateY(-50%)',
-			width: '6px',
-			height: '6px',
-			bg: '#FFFFFF',
-			borderRadius: 'full',
-		}}
-	>
-		<Text
-			fontSize={20}
-			fontWeight={500}
-			fontFamily={FONT_DECORATIVE}
-			paddingLeft={6}
-			paddingRight={2.5}
-			paddingBottom="3px"
+export const PriceTag = (props: Props) => {
+	const dollars = (props.priceCents / 100).toFixed(2);
+	
+	return (
+		<Flex
+			position="relative"
+			borderRadius={5}
+			bg="brand"
+			color="#FFFFFF"
+			style={{
+				clipPath:
+					'polygon(0 50%, 10px 100%, 100% 100%, 100% 0, 10px 0)',
+			}}
+			_after={{
+				content: '""',
+				position: 'absolute',
+				left: '10px',
+				top: '50%',
+				transform: 'translateY(-50%)',
+				width: '6px',
+				height: '6px',
+				bg: '#FFFFFF',
+				borderRadius: 'full',
+			}}
 		>
-			${props.value.toLocaleString()}.00
-			{props.quantity &&
-				props.quantity > 1 &&
-				`(${props.quantity})`}
-		</Text>
-	</Flex>
-);
+			<Text
+				fontSize={20}
+				fontWeight={500}
+				fontFamily={FONT_DECORATIVE}
+				paddingLeft={6}
+				paddingRight={2.5}
+				paddingBottom="3px"
+			>
+				${dollars}
+				{props.quantity &&
+					props.quantity > 1 &&
+					`(${props.quantity})`}
+			</Text>
+		</Flex>
+	);
+};
