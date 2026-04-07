0c7c3045c8e6b8d1ba38439f5e1f931dd20f2845
Nick Heyek <nick@heyek.com>
2026-04-07 12:25:56 -0500

    Remove auto variable for webhook secret (different than local)

--- infrastructure/tf.sh
+++ infrastructure/tf.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 export TF_VAR_stripe_secret_key="$STRIPE_SECRET_KEY"
-export TF_VAR_stripe_webhook_secret="$STRIPE_WEBHOOK_SECRET"
 export TF_VAR_resend_api_key="$RESEND_API_KEY"
 
 terraform "$@"
