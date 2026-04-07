bc3197b6df54202eb90c65002f780ea0eec85f11
Nick Heyek <nick@heyek.com>
2026-04-07 12:14:52 -0500

    Undo stripe provider changes

--- client/src/providers/StripeProvider.tsx
+++ client/src/providers/StripeProvider.tsx
@@ -1,7 +1,7 @@
 import { useBreakpointValue, useToken } from '@chakra-ui/react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import { ReactNode, useMemo } from 'react';
+import { ReactNode } from 'react';
 import { Layout } from '../constants';
 
 const stripePromise = loadStripe(
@@ -31,49 +31,49 @@ export const StripeProvider = ({ children }: Props) => {
 	});
 	const useDarkMode = layout === Layout.COMPACT;
 
-	const options = useMemo(() => ({
-		mode: 'payment' as const,
-		amount: 5000,
-		currency: 'usd',
-		appearance: {
-			labels: 'floating' as const,
-			variables: { spacingUnit: '4px' },
-			rules: {
-				'.Input': {
-					fontSize: '16px',
-					borderRadius: '5px',
-					boxShadow: 'none',
-					backgroundColor: useDarkMode
-						? 'transparent'
-						: gray100,
-					color: useDarkMode ? 'white' : 'black',
-					border: useDarkMode
-						? '1px solid white'
-						: 'none',
-					paddingTop: '10px',
-					paddingBottom: '10px',
-					paddingLeft: '12px',
-				},
-				'.Input:focus': {
-					outline: `1px solid ${useDarkMode ? 'white' : gray400}`,
-					boxShadow: 'none',
-					border: useDarkMode ? '1px solid white' : '0',
-				},
-				'.Error': {
-					fontSize: '15px',
-					paddingTop: '6px',
-				},
-				'.Label': {
-					color: useDarkMode ? gray200 : gray500,
-				},
-			},
-		},
-	}), [useDarkMode, gray100, gray200, gray400, gray500]);
-
 	return (
 		<Elements
 			stripe={stripePromise}
-			options={options}
+			options={{
+				mode: 'payment',
+				amount: 5000,
+				currency: 'usd',
+				appearance: {
+					labels: 'floating',
+					variables: { spacingUnit: '4px' },
+					rules: {
+						'.Input': {
+							fontSize: '16px',
+							borderRadius: '5px',
+							boxShadow: 'none',
+							backgroundColor: useDarkMode
+								? 'transparent'
+								: gray100,
+							color: useDarkMode ? 'white' : 'black',
+							border: useDarkMode
+								? '1px solid white'
+								: 'none',
+							paddingTop: '10px',
+							paddingBottom: '10px',
+							paddingLeft: '12px',
+						},
+						'.Input:focus': {
+							outline: `1px solid ${useDarkMode ? 'white' : gray400}`,
+							boxShadow: 'none',
+							border: useDarkMode
+								? '1px solid white'
+								: '0',
+						},
+						'.Error': {
+							fontSize: '15px',
+							paddingTop: '6px',
+						},
+						'.Label': {
+							color: useDarkMode ? gray200 : gray500,
+						},
+					},
+				},
+			}}
 		>
 			{children}
 		</Elements>
