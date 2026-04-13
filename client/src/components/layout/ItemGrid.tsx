f880a6b929ae15c83a4206cbe89ecd8ac214cd7b
Nick Heyek <nick@heyek.com>
2026-04-13 15:09:45 -0500

    Animation improvements to item grid

--- client/src/components/layout/ItemGrid.tsx
+++ client/src/components/layout/ItemGrid.tsx
@@ -9,7 +9,12 @@ import {
 	STANDARD_GRID_COLUMNS,
 	STANDARD_GRID_GAP,
 } from '@client/constants';
+import { motion } from 'framer-motion';
 import { ReactNode } from 'react';
+
+const MotionBox = motion.create(Box);
+
+const STAGGER_INTERVAL = 0.06;
 
 type Props<T> = {
 	items: T[];
@@ -31,8 +36,26 @@ export const ItemGrid = <T,>(props: Props<T>) => {
 	} = props;
 
 	const maxNumColumns = useBreakpointValue(columns) ?? 1;
-	const numColumns = Math.min(maxNumColumns, items.length || 1);
+	const numColumns = isLoading || items.length === 0
+		? maxNumColumns
+		: Math.min(maxNumColumns, items.length);
 	const placeholderCount = numPlaceholders ?? numColumns * 2;
+
+	const animatedItem = (child: ReactNode, index: number, minWidth?: number) => (
+		<MotionBox
+			minWidth={minWidth}
+			initial={{ opacity: 0, y: -16, rotateZ: -1 }}
+			animate={{ opacity: 1, y: 0, rotateZ: 0 }}
+			transition={{
+				type: 'spring',
+				stiffness: 300,
+				damping: 24,
+				delay: index * STAGGER_INTERVAL,
+			}}
+		>
+			{child}
+		</MotionBox>
+	);
 
 	if (maxNumColumns === 1) {
 		return (
@@ -45,24 +68,13 @@ export const ItemGrid = <T,>(props: Props<T>) => {
 			>
 				{isLoading && (
 					<>
-						<Skeleton
-							width={300}
-							height={300}
-						/>
-						<Skeleton
-							width={300}
-							height={300}
-						/>
+						<Skeleton width={300} height={300} />
+						<Skeleton width={300} height={300} />
 					</>
 				)}
-				{items.map((item, index) => (
-					<Box
-						minWidth={300}
-						key={getItemKey(item, index)}
-					>
-						{renderItem(item, true)}
-					</Box>
-				))}
+				{items.map((item, index) =>
+					animatedItem(renderItem(item, true), index, 300),
+				)}
 			</HStack>
 		);
 	}
@@ -75,19 +87,12 @@ export const ItemGrid = <T,>(props: Props<T>) => {
 			width="fit-content"
 		>
 			{isLoading &&
-				Array.from({ length: placeholderCount }).map(
-					(_, i) => (
-						<Skeleton
-							key={i}
-							height={300}
-						/>
-					),
-				)}
-			{items.map((item, index) => (
-				<Box key={getItemKey(item, index)}>
-					{renderItem(item, false)}
-				</Box>
-			))}
+				Array.from({ length: placeholderCount }).map((_, i) => (
+					<Skeleton key={i} width={300} height={300} />
+				))}
+			{items.map((item, index) =>
+				animatedItem(renderItem(item, false), index),
+			)}
 		</SimpleGrid>
 	);
 };
