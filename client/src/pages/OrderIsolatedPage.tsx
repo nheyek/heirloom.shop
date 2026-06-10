7c807ed944bf0e9738949f8eb734835884426ca2
Nick Heyek <nick@heyek.com>
2026-06-10 14:24:47 -0500

    Isolated order page layout updates

--- client/src/pages/OrderIsolatedPage.tsx
+++ client/src/pages/OrderIsolatedPage.tsx
@@ -1,4 +1,4 @@
-import { Center, Heading, Span, Stack } from '@chakra-ui/react';
+import { Heading, Span, Stack } from '@chakra-ui/react';
 import { OrderPage } from '@client/pages/OrderPage';
 import { FONT_DECORATIVE } from '@client/theme';
 import { useParams } from 'react-router-dom';
@@ -7,23 +7,18 @@ export const OrderIsolatedPage = () => {
 	const { shortId } = useParams<{ shortId: string }>();
 
 	return (
-		<Center>
-			<Stack
-				w={{ base: '100%', md: 'fit-content' }}
-				maxW={1000}
-				py={{ base: 5, md: 8 }}
-				px={5}
-				gap={5}
+		<Stack
+			p={{ base: 5, md: 8 }}
+			gap={5}
+		>
+			<Heading
+				fontSize={36}
+				fontWeight={400}
+				fontFamily={FONT_DECORATIVE}
 			>
-				<Heading
-					fontSize={36}
-					fontWeight={400}
-					fontFamily={FONT_DECORATIVE}
-				>
-					Order <Span fontWeight={500}>{shortId}</Span>
-				</Heading>
-				<OrderPage />
-			</Stack>
-		</Center>
+				Order <Span fontWeight={500}>{shortId}</Span>
+			</Heading>
+			<OrderPage />
+		</Stack>
 	);
 };
