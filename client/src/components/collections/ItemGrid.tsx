f65baa5d79c76b0e0c45cc21790379497f3dda64
Nick Heyek <nick@heyek.com>
2026-07-09 10:57:16 -0500

    Bump item grid skeleton aspect ratio

--- client/src/components/collections/ItemGrid.tsx
+++ client/src/components/collections/ItemGrid.tsx
@@ -61,18 +61,19 @@ export const ItemGrid = <T,>(props: Props<T>) => {
 				m={-5}
 				scrollbarWidth="none"
 			>
-				{isLoading && (
-					<>
-						<Skeleton
-							width={minItemWidth}
-							height={minItemWidth}
-						/>
-						<Skeleton
-							width={minItemWidth}
-							height={minItemWidth}
-						/>
-					</>
-				)}
+				{isLoading ||
+					(true && (
+						<>
+							<Skeleton
+								width={minItemWidth}
+								aspectRatio={0.75}
+							/>
+							<Skeleton
+								width={minItemWidth}
+								aspectRatio={0.75}
+							/>
+						</>
+					))}
 				{items.map((item, index) =>
 					animatedItem(
 						renderItem(item, true),
